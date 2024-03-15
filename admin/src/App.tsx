44aa347ccbf15386b99094d261d7ce865268aa52
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-03-16 00:56:52 +0600

    add solid ui to the admin project

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,7 +1,16 @@
-function App() {
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+
+const App = () => {
     return (
-        <div class="bg-green-500 text-5xl">Hello From Solid xx 4</div>
+        <Tabs defaultValue="account" class="w-[400px]">
+            <TabsList class="grid w-full grid-cols-2">
+                <TabsTrigger value="account">Account</TabsTrigger>
+                <TabsTrigger value="password">Password</TabsTrigger>
+            </TabsList>
+            <TabsContent value="account">Account Tab</TabsContent>
+            <TabsContent value="password">Password Tab</TabsContent>
+        </Tabs>
     );
-}
+};
 
 export default App;
